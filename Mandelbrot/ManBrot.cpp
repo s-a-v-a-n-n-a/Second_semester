e91b35e810eff1ad1df094895da59a6aa04428a3
s-a-v-a-n-n-a <70327418+s-a-v-a-n-n-a@users.noreply.github.com>
2021-03-26 13:03:20 +0300

    More preparations for intrinsics

--- Mandelbrot/ManBrot.cpp
+++ Mandelbrot/ManBrot.cpp
@@ -1,21 +1,29 @@
 #include <TXLib.h>
+#include <emmintrin.h>
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 
-const int horizontal_size = 300;
-const int vertical_size = 300;
+const float horizontal_size = 300;
+const float vertical_size = 300;
 const int radius = 2;
 const int maximum_iterations_amount = 255;
 
-const float dx = 0.001;
-const float epsilon = 0.0000001;
+typedef union array_to_register
+{
+	__m128 xmm;
+	float floats[4];
+
+	__m128i regs;
+	int ints[4];
+} xmm;
 
 typedef struct screen_information
 {
 	RGBQUAD *screen;
-	int horizontal_screen_size;
-	int vertical_screen_size;
+	float horizontal_screen_size;
+	float vertical_screen_size;
 
 	float real_horizontal_size;
 	float real_vertical_size;
@@ -28,20 +36,20 @@ typedef struct screen_information
 }s_information;
 
 s_information *screen_new();
-void screen_set_pixel_color(s_information *screen, long long pixel, char red, char green, char blue, char alpha);
+void screen_set_pixel_color(s_information *screen, long long pixel, char red, char green, char blue);
 void screen_delete(s_information *screen);
 void screen_update_information(s_information *screen);
 
-void four_float_set_one_value (float *array_to, float value);
-void four_int_set_one_value (int *array_to, int value);
-void four_float_multiply (float *array_to, float *array_1, float *array_2);
-void four_float_add (float *array_to, float *array_1, float *array_2);
-void four_float_add_with_int (int *array_to, int *array_1, float *array_2);
-void four_float_sub (float *array_to, float *array_1, float *array_2);
-void four_float_copy (float *array_to, float *array_with);
+inline void four_float_set_one_value (xmm array_to, float value);
+inline void four_int_set_one_value (xmm array_to, int value);
+inline void four_float_multiply (xmm array_to, xmm array_1, xmm array_2);
+inline void four_float_add (xmm array_to, xmm array_1, xmm array_2);
+inline void four_float_add_with_int (xmm array_to, xmm array_1, xmm array_2);
+inline void four_float_sub (xmm array_to, xmm array_1, xmm array_2);
+inline void four_float_copy (xmm array_to, xmm array_with);
 
-void four_float_compare(float *array_to, float *array_with, float *result);
-int four_flat_make_mask(float *float_mask);
+inline void four_float_compare(xmm array_to, xmm array_with, xmm result);
+inline int four_flat_make_mask(xmm float_mask);
 
 void draw_mandelbrot();
 
@@ -72,19 +80,9 @@ s_information *screen_new()
 	return screen;
 }
 
-void screen_set_pixel_color(s_information *screen, long long pixel, char red, char green, char blue, char alpha)
+void screen_set_pixel_color(s_information *screen, long long pixel, char red, char green, char blue)
 {
 	screen->screen[pixel] = {(BYTE) red, (BYTE) green, (BYTE) blue};
-}
-
-float screen_get_dx(s_information *screen)
-{
-	return screen->dx;
-}
-
-float screen_get_dy(s_information *screen)
-{
-	return screen->dy;
 }
 
 void screen_delete(s_information *screen)
@@ -114,63 +112,63 @@ void screen_update_information(s_information *screen)
 }
 
 
-void four_float_set_one_value (float *array_to, float value)
+inline void four_float_set_one_value (xmm array_to, float value)
 {
 	for (int k = 0; k < 4; k++)
-		array_to[k] = value;
+		array_to.floats[k] = value;
 }
 
-void four_int_set_one_value (int *array_to, int value)
+inline void four_int_set_one_value (xmm array_to, int value)
 {
 	for (int k = 0; k < 4; k++)
-		array_to[k] = value;
+		array_to.ints[k] = value;
 }
 
-void four_float_multiply (float *array_to, float *array_1, float *array_2)
+inline void four_float_multiply (xmm array_to, xmm array_1, xmm array_2)
 {
 	for (int k = 0; k < 4; k++)
-		array_to[k] = array_1[k] * array_2[k];
+		array_to.floats[k] = array_1.floats[k] * array_2.floats[k];
 }
 
-void four_float_add (float *array_to, float *array_1, float *array_2)
+inline void four_float_add (xmm array_to, xmm array_1, xmm array_2)
 {
 	for (int k = 0; k < 4; k++)
-		array_to[k] = array_1[k] + array_2[k];
+		array_to.floats[k] = array_1.floats[k] + array_2.floats[k];
 }
 
-void four_float_add_with_int (int *array_to, int *array_1, float *array_2)
+inline void four_float_add_with_int (xmm array_to, xmm array_1, xmm array_2)
 {
 	for (int k = 0; k < 4; k++)
-		array_to[k] = array_1[k] + (int)array_2[k];
+		array_to.ints[k] = array_1.ints[k] + array_2.ints[k];
 }
 
-void four_float_sub (float *array_to, float *array_1, float *array_2)
+inline void four_float_sub (xmm array_to, xmm array_1, xmm array_2)
 {
 	for (int k = 0; k < 4; k++)
-		array_to[k] = array_1[k] - array_2[k];
+		array_to.floats[k] = array_1.floats[k] - array_2.floats[k];
 }
 
-void four_float_copy (float *array_to, float *array_with)
+inline void four_float_copy (xmm array_to, xmm array_with)
 {
 	for (int k = 0; k < 4; k++)
-		array_to[k] = array_with[k];
+		array_to.floats[k] = array_with.floats[k];
 }
 
-void four_float_compare(float *array_to, float *array_with, float *result)
+inline void four_float_compare(xmm array_to, xmm array_with, xmm result)
 {
 	for (int k = 0; k < 4; k++)
-		if (array_to[k] <= array_with[k])
-			result[k] = 1;
+		if (array_to.floats[k] <= array_with.floats[k])
+			result.floats[k] = 1;
 		else
-			result[k] = 0;
+			result.floats[k] = 0;
 }
 
-int four_flat_make_mask(float *float_mask)
+inline int four_flat_make_mask(xmm float_mask)
 {
 	int mask = 0;
 	for (int k = 0; k < 4; k++)
 	{
-		mask = mask * 2 + float_mask[k];
+		mask = mask * 2 + float_mask.floats[k];
 	}
 	return mask;
 }
@@ -183,10 +181,10 @@ void draw_mandelbrot()
 
     s_information *mandelbrot_screen = screen_new();
 
-	long long height = mandelbrot_screen->vertical_screen_size;
-	long long width = mandelbrot_screen->horizontal_screen_size;
+	int height = (int)(mandelbrot_screen->vertical_screen_size);
+	int width = (int)(mandelbrot_screen->horizontal_screen_size);
 
-	float square_radius[4] = {}; four_float_set_one_value(square_radius, radius * radius);
+	xmm square_radius = {}; four_float_set_one_value(square_radius, radius * radius);
 
 	while(true)
 	{
@@ -194,12 +192,6 @@ void draw_mandelbrot()
 			break;
 
 		screen_update_information(mandelbrot_screen);
-
-		float center_x = mandelbrot_screen->center_x;
-		float center_y = mandelbrot_screen->center_y;
-
-		float left_corner_x = mandelbrot_screen->center_x - mandelbrot_screen->real_horizontal_size/2;
-		float left_corner_y = mandelbrot_screen->center_y - mandelbrot_screen->real_vertical_size/2;
 
 		for (int i = 0; i < height; i++)
 		{
@@ -209,41 +201,45 @@ void draw_mandelbrot()
 			long long current_line = i * width;
 			for (int j = 0; j + 3 < width; j += 4)
 			{
-				float points_x_0[4]         = {}; four_float_set_one_value(points_x_0, mandelbrot_screen->dx);
-				float j_value[4]            = {}; four_float_set_one_value(j_value, j);
-				float zero_one_two_three[4] = {0, 1, 2, 3};
-				float left_corners_x[4]     = {}; four_float_set_one_value(left_corners_x, left_corner_x);
+				xmm points_x_0 = {}; four_float_set_one_value(points_x_0, mandelbrot_screen->dx);
+				xmm j_value = {}; four_float_set_one_value(j_value, j);
+				xmm zero_one_two_three = {}; 
+				zero_one_two_three.floats[0] = 0;
+				zero_one_two_three.floats[1] = 1;
+				zero_one_two_three.floats[2] = 2;
+				zero_one_two_three.floats[3] = 3;
+				xmm left_corners_x = {}; four_float_set_one_value(left_corners_x, left_corner_x);
 				
 				four_float_add          (j_value, j_value, zero_one_two_three);
 				four_float_multiply     (points_x_0, points_x_0, j_value);
 				four_float_add          (points_x_0, points_x_0, left_corners_x);
 
-				float points_y_0[4] = {}; four_float_set_one_value(points_y_0, left_corner_y + i * mandelbrot_screen->dy);
+				xmm points_y_0 = {}; four_float_set_one_value(points_y_0, left_corner_y + i * mandelbrot_screen->dy); 
 
-				float points_x[4] = {}; four_float_copy(points_x, points_x_0);
-	 			float points_y[4] = {}; four_float_copy(points_y, points_y_0);
+				xmm points_x = {}; four_float_copy(points_x, points_x_0);
+	 			xmm points_y = {}; four_float_copy(points_y, points_y_0);
 
-				float place[4] = {}; four_float_set_one_value(place, 1);
-				int counter[4] = {}; four_int_set_one_value(counter, 0);
+				xmm place = {}; four_float_set_one_value(place, 1);
+				xmm counter = {}; four_int_set_one_value(counter, 0);
 				int common_counter = 0;
 
 				while (common_counter < maximum_iterations_amount)
 				{
-					float square_points_x[4] = {}; four_float_multiply (square_points_x, points_x, points_x);
-					float square_points_y[4] = {}; four_float_multiply (square_points_y, points_y, points_y);
+					xmm square_points_x = {}; four_float_multiply (square_points_x, points_x, points_x);
+					xmm square_points_y = {}; four_float_multiply (square_points_y, points_y, points_y);
 
-					float tmp_points_x[4] = {}; 
+					xmm tmp_points_x = {};
 					four_float_sub (tmp_points_x, square_points_x, square_points_y); 
 					four_float_add (tmp_points_x, tmp_points_x, points_x_0);
 
-					float tmp_points_y[4]    = {}; four_float_multiply (tmp_points_y, points_x, points_y);
+					xmm tmp_points_y = {}; four_float_multiply (tmp_points_y, points_x, points_y);
 					four_float_add (tmp_points_y, tmp_points_y, tmp_points_y); four_float_add (tmp_points_y, tmp_points_y, points_y_0);
 
-					float tmp_points_square_x[4] = {}; four_float_multiply (tmp_points_square_x, tmp_points_x, tmp_points_x);
+					xmm tmp_points_square_x = {}; four_float_multiply (tmp_points_square_x, tmp_points_x, tmp_points_x);
 
-					float tmp_points_square_y[4] = {}; four_float_multiply (tmp_points_square_y, tmp_points_y, tmp_points_y);
+					xmm tmp_points_square_y = {}; four_float_multiply (tmp_points_square_y, tmp_points_y, tmp_points_y);
 
-					float distances_from_center[4] = {}; four_float_add (distances_from_center, tmp_points_square_x, tmp_points_square_y);
+					xmm distances_from_center = {}; four_float_add (distances_from_center, tmp_points_square_x, tmp_points_square_y);
 
 					four_float_compare  (distances_from_center, square_radius, place);
 					int mask = four_flat_make_mask(place);
@@ -257,7 +253,7 @@ void draw_mandelbrot()
 				}
 
 				for (int k = 0; k < 4; k++)
-					screen_set_pixel_color(mandelbrot_screen, current_line + j + k, (char)counter[k], 128, 255 - (char)counter[k], 255);
+					screen_set_pixel_color(mandelbrot_screen, current_line + j + k, (char)counter.ints[k], 128, 255 - (char)counter.ints[k]);
 			}
 		}
 		printf ("\t\r%.0lf", txGetFPS());
